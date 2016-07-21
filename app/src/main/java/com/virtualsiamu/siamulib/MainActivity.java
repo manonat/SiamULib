f62985c235f558c17b9b172c62bad971a9fe7cb7
manonat <buddhacomic@hotmail.com>
2016-07-21 20:20:56 +0700

    version 1

---
+++ app/src/main/java/com/virtualsiamu/siamulib/MainActivity.java
@@ -0,0 +1,27 @@
+package com.virtualsiamu.siamulib;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void browser1(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.siamulib.com/siamulib"));
+        startActivity(browserIntent);
+    }
+    public void siamulibcilck (View view){
+        Intent  siamulibIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.siamulib.com/siamulib"));
+        startActivity(siamulibIntent);
+    }
+
+}
